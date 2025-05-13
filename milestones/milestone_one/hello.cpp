69f36572243175f54c71ddd39ae931066f56ae02
Sejal Jadhav <sejal.jadhav@castlabs.com>
2025-05-13 12:46:34 +0200

    Milestone_one implementation

---
+++ milestones/milestone_one/hello.cpp
@@ -0,0 +1,12 @@
+#include "hello.h"
+#include <iostream>
+
+void print_hello() {
+    std::cout << "Hello World from Sejal" << std::endl;
+}
+
+// Main function for milestone 01
+int main() {
+    print_hello();
+    return 0;
+}
---
+++ milestones/milestone_one/hello.h
@@ -0,0 +1,7 @@
+#ifndef MILESTONE_HELLO_H
+#define MILESTONE_HELLO_H
+
+// Function to print hello message
+void print_hello();
+
+#endif // MILESTONE_HELLO_H
